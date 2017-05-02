3a870035b71769c761942f966c9ac192f2dcbc1f
Ashley Williams <ashley666ashley@gmail.com>
2017-05-02 17:35:15 +0300

    add initial impl

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+extern crate regex;
+
+use std::io::{self, BufRead};
+use std::collections::BTreeMap;
+
+use regex::Regex;
+
+fn main() {
+  let is_a_word = Regex::new(r"[a-z']+").unwrap();
+  let mut counts: BTreeMap<String, isize> = BTreeMap::new();
+
+  let mut buffer = String::new();
+  let stdin = io::stdin();
+  let mut handle = stdin.lock();
+
+  for line_result in handle.lines() {
+    let line = line_result.unwrap().to_lowercase();
+    let words: Vec<_> = is_a_word.find_iter(&line)
+      .map(|word| word.as_str().to_string())
+      .collect();
+
+    for word in words {
+      *counts.entry(word).or_insert(0) += 1;
+    }
+    print_counts(&counts);
+  }
+}
+
+fn print_counts(counts: &BTreeMap<String, isize>) {
+  for (word, count) in counts.iter() {
+    println!("{}: {}", word, count);
+  }
+}
